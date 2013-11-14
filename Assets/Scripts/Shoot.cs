50a62c4f16492d825ccf6c1c31d74de9e307dfcb
Animii <jonathan.sauerer@gmx.de>
2013-11-14 21:17:01 +0100

    Shooting

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public WeaponInfo weaponInfo;
+	public bool isKI;
+	public bool isPlayer;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	void CheckInput()
+	{
+		if(Input.GetButtonDown("Fire1"))
+		{	
+
+				Shooting();
+
+		}	
+	}
+
+
+
+	void Shooting()
+	{
+		GameObject tmpBullet = (GameObject) Instantiate(weaponInfo.bullet,weaponInfo.bulletOut.position,weaponInfo.bulletOut.rotation);
+		tmpBullet.GetComponent<Rigidbody2D>().AddForce(weaponInfo.bulletOut.right * weaponInfo.bulletForce);
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(isPlayer)	
+			CheckInput();
+	}
+	
+}
+
+
---
+++ Assets/Scripts/WeaponInfo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponInfo : MonoBehaviour  {
+	public float force = 1;
+	public float fireDelay = 0;
+	public Transform bulletOut;
+	public GameObject bullet;
+	public float bulletForce = 1;
+}
