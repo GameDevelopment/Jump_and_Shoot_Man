721c0e602e9088e6a77e6d752b5b0724a03dcef9
Animii <jonathan.sauerer@gmx.de>
2013-11-16 04:21:39 +0100

    Bullet implementation

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	public float distroyTime;
+	public float dmg;
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject,distroyTime);
+	}
+	
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Enemy")
+			coll.gameObject.SendMessage("ApplyDamage", dmg);
+
+		Destroy(gameObject);
+	}
+}
